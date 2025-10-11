ed0ac09ce658d252aa98ea896005ae986f10108a
Ayrton Chilibeck <git@blobfish.icu>
2025-10-11 16:19:43 -0600

    fix: globally install dragon-runner
    Dragon runner previously tried to install in a venv. This patch
    makes sure that the global install works

--- DragonRunner/build_tag.sh
+++ DragonRunner/build_tag.sh
@@ -3,4 +3,5 @@ source ../VERSIONS.env
 docker build --push \
   -t ghcr.io/cmput415/dragon-runner:$DRAGON_RUNNER_VERSION \
   --build-arg "DRAGON_RUNNER_VERSION=$DRAGON_RUNNER_VERSION" \
+  --build-arg "LLVM_VERSION=$LLVM_VERSION" \
   .
